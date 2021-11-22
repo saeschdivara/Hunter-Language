0a3011c62a5743d69afded95d85dbe82b6866f8b
Sascha Häusler <sascha.haeusler@bitvoodoo.ch>
2021-11-22 21:37:52 +0100

    Add parsing for function call

--- Compiler/testing/Parser.cpp
+++ Compiler/testing/Parser.cpp
@@ -8,18 +8,30 @@ using namespace Hunter::Compiler;
 
 TEST_CASE( "Simple instructions are parsed", "[parser]" ) {
 
-    Parser parser;
+    SECTION("print instruction") {
+        Parser parser;
 
-    auto * expr = parser.ParseLine(" print(\"Hello 8\\n\")");
-    REQUIRE( dynamic_cast<PrintExpression *>(expr) );
+        auto * expr = parser.ParseLine(" print(\"Hello 8\\n\")");
+        REQUIRE( dynamic_cast<PrintExpression *>(expr) );
 
-    auto * printExpr = dynamic_cast<PrintExpression *>(expr);
-    REQUIRE( dynamic_cast<FunctionCallExpression *>(printExpr->GetInput()) );
+        auto * printExpr = dynamic_cast<PrintExpression *>(expr);
+        REQUIRE( dynamic_cast<FunctionCallExpression *>(printExpr->GetInput()) );
 
-    auto * funcCallExpr = dynamic_cast<FunctionCallExpression *>(printExpr->GetInput());
-    REQUIRE(funcCallExpr->GetParameters().size() == 1);
+        auto * funcCallExpr = dynamic_cast<FunctionCallExpression *>(printExpr->GetInput());
+        REQUIRE(funcCallExpr->GetParameters().size() == 1);
 
-    auto * strExpression = dynamic_cast<StringExpression *>(funcCallExpr->GetParameters().at(0));
-    REQUIRE(strExpression);
-    REQUIRE(strExpression->GetString() == "Hello 8\n");
+        auto * strExpression = dynamic_cast<StringExpression *>(funcCallExpr->GetParameters().at(0));
+        REQUIRE(strExpression);
+        REQUIRE(strExpression->GetString() == "Hello 8\n");
+    }
+
+    SECTION("function call instruction") {
+        Parser parser;
+
+        auto * expr = parser.ParseLine(" foo()");
+        REQUIRE( dynamic_cast<FunctionCallExpression *>(expr) );
+
+        auto * funcCallExpr = dynamic_cast<FunctionCallExpression *>(expr);
+        REQUIRE(funcCallExpr->GetParameters().empty());
+    }
 }
